83d8d6e6b9c0b441bd65abf6fb52a900c39aeff8
Dominic Kempf <dominic.kempf@iwr.uni-heidelberg.de>
2024-01-08 14:13:48 +0100

    Fix hook-whisper.py after changes to whisper dependencies

--- hook-whisper.py
+++ hook-whisper.py
@@ -1,7 +1,4 @@
-from PyInstaller.utils.hooks import collect_data_files, copy_metadata
-
-# The transformers package inspects metadata of its dependencies
-datas = copy_metadata("transformers", recursive=True)
+from PyInstaller.utils.hooks import collect_data_files
 
 # We explicitly bundle whisper assets
-datas += collect_data_files("whisper", subdir="assets")
+datas = collect_data_files("whisper", subdir="assets")
